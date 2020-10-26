f5e0381054f7d130728977d836732ee96d7cf509
j-otterbox <jasonotter17@gmail.com>
2020-10-25 19:42:29 -0700

    merged over to github repo from local

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 import boto3
 import botocore
 import logging
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, request, render_template
+from botocore.exceptions import ClientError
 
 app = Flask(__name__)
 
@@ -13,37 +14,14 @@ def hello_world():
   # Let's use Amazon S3
   s3 = boto3.resource('s3')
 
-  buckets = []
-
-  # Print out bucket names
-  for bucket in s3.buckets.all():
-    buckets.append(bucket.name)
-
-  #Upload a new file
-  #data = open('s3-test-img-1.jpeg', 'rb')
-  #s3.Bucket('349-food-fights').put_object(Key='s3-test-img-2.jpeg', Body=data)
-
-  app.logger.info(buckets)
+  # Upload a new file
+  data = open('.\static\imgs\portfolio\s3-test-img-1.jpeg', 'rb')
+  s3.Bucket('349-food-fights').put_object(Key='chicken-pot-pie.jpg', Body=data)
 
   return render_template('index.html', name='home')
 
-@app.route('/post')
-def samplePost():
-  return render_template('post.html', name='post')
 
-
-@app.route('/photos/grilled_cheese')
-def getGrilledCheese():
-
-  s3 = boto3.resource('s3')
-  KEY = 'test_album/s3-test-img-3.jpeg'
-
-  try:
-    s3.Bucket('349-food-fights').download_file(KEY, 'grilled_cheese.jpg')
-  except botocore.exceptions.ClientError as e:
-    if e.response['Error']['Code'] == "404":
-        print("The object does not exist.")
-    else:
-        raise
-
-  return 'did it work?'
+@app.route('/fetch-test', methods=['GET'])
+def createNewPost():
+  message = {'greeting':'hello from the fetch-test endpoint!'}
+  return jsonify(message)  # serialize and use JSON headers
