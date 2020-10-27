3188d00bebeaa7166a3de099153ff29729b67505
Jazmin Esqueda <jazminesqueda@csu.fullerton.edu>
2020-10-26 21:14:59 -0700

    Upload

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 import boto3
 import botocore
 import logging
-from flask import Flask, jsonify, request, render_template
-from botocore.exceptions import ClientError
+import sqlite3
+from flask import Flask, jsonify, render_template, g, request
 
 app = Flask(__name__)
+app.config.from_envvar('APP_CONFIG')
+DATABASE = 'posts.db'
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -12,16 +14,258 @@ logging.basicConfig(level=logging.DEBUG)
 def hello_world():
 
   # Let's use Amazon S3
+  s3 = boto3.resource('s3',
+         aws_access_key_id='AKIA274WOECWYA2IANEC',
+         aws_secret_access_key= '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')
+
+  buckets = []
+
+  # Print out bucket names
+  for bucket in s3.buckets.all():
+    buckets.append(bucket.name)
+
+  #Upload a new file
+  #data = open('s3-test-img-1.jpeg', 'rb')
+  #s3.Bucket('349-food-fights').put_object(Key='s3-test-img-2.jpeg', Body=data)
+
+  app.logger.info(buckets)
+
+
+  all_posts = query_db('SELECT * FROM posts;')
+  all_comments= query_db('SELECT * FROM comments;')
+  return render_template('index.html', name='home',post=jsonify(all_posts),comments=jsonify(all_comments))
+
+@app.route('/post')
+def samplePost():
+  return render_template('post.html', name='post')
+
+
+@app.route('/photos/grilled_cheese')
+def getGrilledCheese():
+
   s3 = boto3.resource('s3')
+  KEY = 'test_album/s3-test-img-3.jpeg'
 
-  # Upload a new file
-  data = open('.\static\imgs\portfolio\s3-test-img-1.jpeg', 'rb')
-  s3.Bucket('349-food-fights').put_object(Key='chicken-pot-pie.jpg', Body=data)
+  try:
+    s3.Bucket('349-food-fights').download_file(KEY, 'grilled_cheese.jpg')
+  except botocore.exceptions.ClientError as e:
+    if e.response['Error']['Code'] == "404":
+        print("The object does not exist.")
+    else:
+        raise
 
-  return render_template('index.html', name='home')
+  return 'did it work?'
+
+# -------------------------------------------------------
 
 
-@app.route('/fetch-test', methods=['GET'])
-def createNewPost():
-  message = {'greeting':'hello from the fetch-test endpoint!'}
-  return jsonify(message)  # serialize and use JSON headers
+def make_dicts(cursor, row):
+  return dict((cursor.description[idx][0], value)
+              for idx, value in enumerate(row))
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(app.config['DATABASE'])
+        db.row_factory = make_dicts
+    return db
+    
+    
+#returns 200 OK response if successful  
+def send_ok200(data):
+	response = jsonify(data)
+	response.status_code = 200
+	response.mimetype = "application/json"
+	return response
+	
+	
+	
+# error message
+@app.errorhandler(404)	
+def page_not_found(error = None):
+	message = { 
+	'status: ': 404,
+	'message: ': 'Not Found: ' + request.url
+	}
+	response = jsonify(message)
+	response.status_code = 404
+	response.mimetype = "application/json"
+	return response
+	
+	
+# conflict message
+def conflict_409():
+	message = { 
+	'status: ': 409,
+	'message: ': 'Conflict: ' + request.url
+	}
+	response = jsonify(message)
+	response.status_code = 409
+	response.mimetype = "application/json"
+	return response
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+@app.cli.command('init')
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('posts.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+
+@app.route('/posts/add_post', methods=['GET', 'POST'])
+def add_post():
+	if request.method == 'POST':
+	
+		post_id = request.form['post_id']
+		post_title = request.form['post_title']
+		post_text = request.form['post_text']
+		post_community = request.form['post_community']
+		post_url = request.form['post_url']
+		post_user_name = request.form['post_user_name']
+		post_date = request.form['post_date']
+		
+		# checks to see if post with this ID already exists
+		query= "SELECT post_title FROM posts WHERE post_id =?;"
+		check = query_db(query, (post_id,))
+		if check == []:
+			
+	
+			# add input to list
+			input_args = []
+			input_args.append(post_id)
+			input_args.append(post_title)
+			input_args.append(post_text)
+			input_args.append(post_community)
+			input_args.append(post_url)
+			input_args.append(post_user_name)
+			input_args.append(post_date)
+			
+			
+			query= "INSERT INTO posts(post_id, post_title, post_text, post_community, post_url, post_user_name, post_date) VALUES(?,?,?,?,?,?,?);"
+			conn = sqlite3.connect(DATABASE)
+			conn.execute(query, input_args)
+			conn.commit()
+			conn.close()
+		
+		else: return conflict_409()
+		
+		# message for successful post upload
+		message = {
+			'Post ID: ' : post_id,
+			'Post Title: ': post_title,
+			'Post Text: ': post_text,
+			'Post Community: ': post_community, 
+			'Post URL: ': post_url,
+			'Post User Name: ': post_user_name,
+			'Post Date': post_date
+		}
+		
+
+		return send_ok200(message)
+
+
+# get post by id
+@app.route('/posts/get_post', methods=['GET'])
+def get_post():
+	query_parameters = request.args
+	post_id = query_parameters.get('post_id')
+	get_post = query_db('SELECT post_id, post_title, post_community, post_user_name, post_date FROM posts WHERE post_id =?;', (post_id))
+	# checks if post exists
+	if get_post == []:
+			return page_not_found(404)
+	return send_ok200(get_post)
+
+
+
+
+@app.route('/comments/<int:p_id>/top/<int:max>', methods=['GET', 'POST'])
+def get_top(p_id,max):
+	gc = query_db('SELECT * FROM comments WHERE p_id = ? ORDER BY comment_date DESC LIMIT ?;', (p_id,max))
+
+	if gc == []:
+		return page_not_found(404)
+	return send_ok200(gc)
+   # if the above doesnt work try this:
+  #  return jsonify(gc)
+
+@app.route('/posts/get_Recent', methods=['GET'])
+def get_Recent():
+	query_parameters = request.args
+	n = query_parameters.get('n')
+	get_post = query_db('SELECT post_id, post_title, post_community, post_user_name, post_date FROM posts ORDER BY post_date DESC LIMIT ?;', (n))
+	# checks if empty
+	if get_post == []:
+			return page_not_found(404)
+	return send_ok200(get_post)
+   # if the above doesnt work try this:
+  #  return jsonify(get_post)
+
+
+@app.route('/comments/get_Recent', methods=['GET'])
+def get_RecentComments():
+	query_parameters = request.args
+	n = query_parameters.get('n')
+	get_post = query_db('SELECT comment_text, comment_user_name, comment_date FROM comments ORDER BY post_date DESC LIMIT ?;', (n))
+	# checks if empty
+	if get_post == []:
+			return page_not_found(404)
+	return send_ok200(get_post)
+
+@app.route('/comments/add_comment', methods=['GET', 'POST'])
+def add_comment():
+	if request.method == 'POST':
+		p_id = request.form['p_id']
+		comment_text = request.form['post_text']
+		comment_user_name = request.form['post_user_name']
+		comment_date = request.form['post_date']
+		
+		# checks to see if post with this ID already exists
+		query= "SELECT comment_user_name FROM comments WHERE p_id =?;"
+		check = query_db(query, (post_id,))
+		if check == []:
+			
+	
+			# add input to list
+			input_args = []
+			input_args.append(p_id)
+			input_args.append(comment_text)
+			input_args.append(comment_user_name)
+			input_args.append(comment_date)
+			
+			
+			query= "INSERT INTO comments(p_id, comment_text, comment_user_name, comment_date) VALUES(?,?,?,?);"
+			conn = sqlite3.connect(DATABASE)
+			conn.execute(query, input_args)
+			conn.commit()
+			conn.close()
+		
+		else: return conflict_409()
+		
+		# message for successful post upload
+		message = {
+			'Post ID: ' : post_id,
+			'Comment Text: ': comment_text, 
+			'Comment URL: ': comment_url,
+			'Comment User Name: ': comment_user_name,
+			'Comment Date': comment_date
+		}
+		
+
+		return send_ok200(message)
---
+++ posts.sql
@@ -0,0 +1,57 @@
+-- $ sqlite3 db1.db <postssq
+
+-- PRAGMA foreign_keys=ON;
+BEGIN TRANSACTION;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS comments;
+
+
+
+CREATE TABLE  posts (
+	post_id INTEGER primary key,
+	post_title VARCHAR,
+	post_text VARCHAR,
+	post_community VARCHAR,
+	post_url VARCHAR,
+	post_user_name VARCHAR,
+	post_date VARCHAR,
+	UNIQUE(post_id)
+);
+
+
+CREATE TABLE  comments(
+	ID INTEGER PRIMARY KEY,
+	p_id INTEGER,
+	comment_text VARCHAR,
+	comment_user_name VARCHAR,
+	comment_date VARCHAR,
+	FOREIGN KEY(p_id) REFERENCES posts(post_id)
+);
+
+INSERT INTO posts (post_id,post_title,post_text,post_community,post_url,post_user_name,post_date) VALUES (1,"Title 1","Text1","Community1","Post URL 1","User1","0/0/0");
+INSERT INTO posts (post_id,post_title,post_text,post_community,post_url,post_user_name,post_date) VALUES (2,"Title 2","Text2","Community2","Post URL 2","User2","0/0/0");
+INSERT INTO posts (post_id,post_title,post_text,post_community,post_url,post_user_name,post_date) VALUES (3,"Title 3","Text3","Community3","Post URL 3","User3","0/0/0");
+INSERT INTO posts (post_id,post_title,post_text,post_community,post_url,post_user_name,post_date) VALUES (4,"Title 4","Text4","Community4","Post URL 4","User4","0/0/0");
+INSERT INTO posts (post_id,post_title,post_text,post_community,post_url,post_user_name,post_date) VALUES (5,"Title 5","Text5","Community5","Post URL 5","User5","0/0/0");
+INSERT INTO posts (post_id,post_title,post_text,post_community,post_url,post_user_name,post_date) VALUES (6,"Title 1","Text1","Community1","Post URL 1","User2","1/0/0");
+INSERT INTO posts (post_id,post_title,post_text,post_community,post_url,post_user_name,post_date) VALUES (7,"Title 2","Text2","Community1","Post URL 2","User3","1/0/0");
+INSERT INTO posts (post_id,post_title,post_text,post_community,post_url,post_user_name,post_date) VALUES (8,"Title 3","Text3","Community1","Post URL 3","User4","1/0/0");
+INSERT INTO posts (post_id,post_title,post_text,post_community,post_url,post_user_name,post_date) VALUES (9,"Title 4","Text4","Community1","Post URL 4","User4","1/0/0");
+INSERT INTO posts (post_id,post_title,post_text,post_community,post_url,post_user_name,post_date) VALUES (10,"Title 5","Text5","Community1","Post URL 5","User5","1/0/0");
+
+
+
+
+INSERT INTO comments(p_id,comment_text, comment_user_name, comment_date) VALUES(1,'Comment 1', 'Comment text 1', '10/5/2020');
+INSERT INTO comments(p_id,comment_text, comment_user_name, comment_date) VALUES(1,'Comment 2', 'Comment text 2', '10/5/2020');
+INSERT INTO comments(p_id,comment_text, comment_user_name, comment_date) VALUES(1,'Comment 3', 'Comment text 3', '10/5/2020');
+INSERT INTO comments(p_id,comment_text, comment_user_name, comment_date) VALUES(1,'Comment 4', 'Comment text 4', '10/5/2020');
+INSERT INTO comments(p_id,comment_text, comment_user_name, comment_date) VALUES(1,'Comment 5', 'Comment text 5', '10/5/2020');
+INSERT INTO comments(p_id,comment_text, comment_user_name, comment_date) VALUES(2,'Comment 1', 'Comment text 1', '10/5/2020');
+INSERT INTO comments(p_id,comment_text, comment_user_name, comment_date) VALUES(2,'Comment 2', 'Comment text 2', '10/5/2020');
+INSERT INTO comments(p_id,comment_text, comment_user_name, comment_date) VALUES(2,'Comment 3', 'Comment text 3', '10/5/2020');
+INSERT INTO comments(p_id,comment_text, comment_user_name, comment_date) VALUES(2,'Comment 4', 'Comment text 4', '10/5/2020');
+INSERT INTO comments(p_id,comment_text, comment_user_name, comment_date) VALUES(2,'Comment 5', 'Comment text 5', '10/5/2020');
+
+
+COMMIT;
